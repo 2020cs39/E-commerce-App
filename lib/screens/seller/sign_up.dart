6a15318f46c6bf8c12ddc3989fd04ccfb538e7f5
Afzal <afzalashraf814@gmail.com>
2023-02-28 00:14:41 +0500

    added home screen

--- lib/screens/seller/sign_up.dart
+++ lib/screens/seller/sign_up.dart
@@ -3,7 +3,6 @@ import 'package:ecommerce_app/widgets/input_field.dart';
 import 'package:ecommerce_app/widgets/page_header.dart';
 import 'package:flutter/material.dart';
 
-
 import '../../util/responsive.dart';
 
 class SignUpSeller extends StatelessWidget {
@@ -20,50 +19,32 @@ class SignUpSeller extends StatelessWidget {
     TextEditingController city = TextEditingController();
     TextEditingController address = TextEditingController();
 
-    return Expanded(
-      child:Column(
-        
-        children:<Widget> [
-
+    return ListView(
+      children: <Widget>[
         Image.asset('assets/images/logo.jpg'),
         SizedBox(height: SizeConfig.verticalBlockSize! * 1.5),
-
         const PageHeading("Sign Up"),
         SizedBox(height: SizeConfig.verticalBlockSize! * 2),
-
         InputField(controller: fullName, labelHint: "Full name"),
         SizedBox(height: SizeConfig.verticalBlockSize! * 2),
-
         InputField(controller: username, labelHint: "Email"),
         SizedBox(height: SizeConfig.verticalBlockSize! * 2),
-
         InputField(controller: password, labelHint: "Password"),
         SizedBox(height: SizeConfig.verticalBlockSize! * 2),
-
         InputField(controller: phone, labelHint: "Phone no."),
         SizedBox(height: SizeConfig.verticalBlockSize! * 2),
-
         InputField(controller: cnicNumber, labelHint: "CNIC"),
         SizedBox(height: SizeConfig.verticalBlockSize! * 2),
-
         InputField(controller: address, labelHint: "Address"),
         SizedBox(height: SizeConfig.verticalBlockSize! * 2),
-
         InputField(controller: city, labelHint: "City"),
         SizedBox(height: SizeConfig.verticalBlockSize! * 2),
-
-
         SizedBox(height: SizeConfig.verticalBlockSize! * 3),
         MyButton(
           buttonText: "Sign Up",
-          onTap: () {
-          },
+          onTap: () {},
         )
-      
-        ],
-      )
-      )
-      
-    ;
+      ],
+    );
   }
 }
