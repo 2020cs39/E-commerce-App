5ea5658d2e6603c7ce9055edb22e8800e670d16a
Afzal <afzalashraf814@gmail.com>
2023-03-01 21:09:32 +0500

    fixed exception

--- lib/widgets/grid_view.dart
+++ lib/widgets/grid_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
 import '../util/app_style.dart';
 import '../util/data.dart';
+import '../util/responsive.dart';
 
 class TwoColumnGridView extends StatelessWidget {
   const TwoColumnGridView({
@@ -10,72 +10,73 @@ class TwoColumnGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: GridView.count(
-        crossAxisCount: 2,
-        shrinkWrap: true,
-        physics: const ScrollPhysics(),
-        children: justForYouItems
-            .map((item) => InkWell(
-                  onTap: () {},
-                  child: Container(
-                    height: 400,
-                    margin: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: eGrey, width: 0.5),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: 100,
-                            width: 185,
-                            child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: Image.network(
-                                  item["src"],
-                                  fit: BoxFit.contain,
-                                )),
-                          ),
-                          Text(item["title"],
-                              textAlign: TextAlign.center,
-                              overflow: TextOverflow.ellipsis,
-                              style: const TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
+    SizeConfig().init(context);
+    return GridView.count(
+      crossAxisCount: 2,
+      shrinkWrap: true,
+      physics: const ScrollPhysics(),
+      children: justForYouItems
+          .map((item) => InkWell(
+                onTap: () {},
+                child: Container(
+                  height: 500,
+                  margin: const EdgeInsets.all(5),
+                  padding: const EdgeInsets.symmetric(horizontal: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: eGrey, width: 0.5),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: SizedBox(
+                    height: 500,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: SizeConfig.verticalBlockSize! * 13,
+                          width: SizeConfig.horizontalBlockSize! * 75,
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                item["src"],
+                                fit: BoxFit.cover,
                               )),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            children: [
-                              const Icon(Icons.star,
-                                  color: Colors.orange, size: 20),
-                              Text("${item["averageRating"]} /5"),
-                              const SizedBox(
-                                width: 5,
-                              ),
-                              Text("(${item["totalReviews"]})"),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text("${item["price"]} PKR",
-                              style: const TextStyle(
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.orange)),
-                        ],
-                      ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(item["title"],
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            )),
+                        Row(
+                          children: [
+                            const Icon(Icons.star,
+                                color: Colors.orange, size: 20),
+                            Text("${item["averageRating"]} /5"),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            Text("(${item["totalReviews"]})"),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text("${item["price"]} PKR",
+                            style: const TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.orange)),
+                      ],
                     ),
                   ),
-                ))
-            .toList(),
-      ),
+                ),
+              ))
+          .toList(),
     );
   }
 }
